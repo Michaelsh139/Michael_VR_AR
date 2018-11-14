221ac549d25147da07163822468eb6378acff454
Michaelsh139 <michaelsh139@gmail.com>
2018-11-14 20:10:09 +0200

    First commit

---
+++ Assets/Scripts/DestoryOverTime.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryOverTime : MonoBehaviour {
+
+    [SerializeField] private float lifeTime = 5;
+
+    private void Awake()
+    {
+        Destroy(this.gameObject, lifeTime);
+    }
+}
---
+++ Assets/Scripts/RifleBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RifleBehavior : MonoBehaviour {
+
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform spawnShot;
+    [SerializeField] private float shotForce = 1000;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            GameObject newBullet = Instantiate(bullet, spawnShot.position, transform.rotation);
+            newBullet.GetComponent<Rigidbody>().AddForce(newBullet.transform.forward * shotForce);
+        }
+    }
+}
